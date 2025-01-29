ae6d06861b2e81d2da601515276545e25c578364
Ashishat404 <138343378+Ashishat404@users.noreply.github.com>
2025-01-29 19:31:12 +0530

    Update 06 Variable,Datatype.py

--- 06 Variable,Datatype.py
+++ 06 Variable,Datatype.py
@@ -1,1 +1,12 @@
 
+# Variables And Data Types
+a= complex(1,2)
+print(a)
+b=True
+print(b)
+c="Ashish"
+d=None
+print("the type of a is ", type(a))
+print("the type of b is ", type(b))
+print("the type of c is ", type(c))
+print("the type of d is ", type(d))
