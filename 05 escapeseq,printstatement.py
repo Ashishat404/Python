56acae40a4a04fbaeeaff9b2bfe7f76d5f35268b
Ashishat404 <138343378+Ashishat404@users.noreply.github.com>
2025-01-29 19:29:29 +0530

    Add files via upload

---
+++ 05 escapeseq,printstatement.py
@@ -0,0 +1,20 @@
+# Escape Sequence , Print Statement , Comments 
+
+# hi this is a single line comment 
+
+'''
+hi this is a
+multi line comment with triple single-quotes
+'''
+
+"""Here this is a multiline comment with triple-quotes """
+
+# this is also a multiline comment 
+# using replit and vscode like modern ide's
+
+print("i am a good boy and you \"too a good \" \n boy , girl ")
+
+
+print("hello",6,9, sep="~") # sep is used to seperate the values
+
+print("hello",8,77, end="TheEnd") # end is used to end the line with the given value like here i've used TheEnd
