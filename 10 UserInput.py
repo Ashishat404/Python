d8518bb90a16714d8d3978f755941f929727dd54
Ashishat404 <138343378+Ashishat404@users.noreply.github.com>
2025-01-30 21:23:27 +0530

    Update 10 UserInput.py

--- 10 UserInput.py
+++ 10 UserInput.py
@@ -1,1 +1,8 @@
 
+a = input("enter your input: ")
+print ("The input value is :", a)
+
+x = input("Enter first number: ")
+y = input("Enter second number: ")
+print (x+y)   # Conactenation 
+print(int (x) + int (y) )    # Addition 
