13a35310584440bf008bca638c222777fc28ae3b
Ashishat404 <138343378+Ashishat404@users.noreply.github.com>
2025-01-30 10:25:31 +0530

    Update 07 calculator.py

--- 07 calculator.py
+++ 07 calculator.py
@@ -1,1 +1,8 @@
 
+a= 50
+b= 3
+
+print("the Value of " ,a ,"+", b ,"is : ",a+b)
+print("the Value of " ,a ,"-", b ,"is : ",a-b)
+print("the Value of " ,a ,"*", 3 ,"is : ",a*b)
+print("the Value of " ,a ,"/", 3 ,"is : ",a/b)
