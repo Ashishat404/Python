b01736ac0bc2a3544e62e394c2fda4322c2507e5
Ashishat404 <138343378+Ashishat404@users.noreply.github.com>
2025-01-30 10:27:07 +0530

    Update 09 Typecasting.py

--- 09 Typecasting.py
+++ 09 Typecasting.py
@@ -1,1 +1,17 @@
+# a="1"
+a=1
+# b="2"  # even if you don't comment it out you will still recieve the value 3 
+b=2
 
+print(a + b)
+
+c="2"
+d="5"
+print(int(c) + int(d)) # this can be used when you have integer in the string 
+                       # At first it converts string into integer and then prints it
+
+# Implicit Typecasting
+e= 1.9
+f= 8
+
+print (e + f)
