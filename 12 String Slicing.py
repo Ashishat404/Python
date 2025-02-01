deae2f26a91853175cede348de29cec7e99e8727
Ashishat404 <138343378+Ashishat404@users.noreply.github.com>
2025-02-01 21:47:11 +0530

    Update 12 String Slicing.py

--- 12 String Slicing.py
+++ 12 String Slicing.py
@@ -1,1 +1,6 @@
+names = "harry,ashish"
+fruit = "mango"
+print(names[0:5])       # prints the length including 0 but not 5 
+print(len(names))       # Gives the length of a string 
+print(fruit[0:4])       # prints the length including 0 but not 4
 
