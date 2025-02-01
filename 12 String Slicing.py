7ea9394c69dee9e45a6fdf027906e21e46d7d984
Ashishat404 <138343378+Ashishat404@users.noreply.github.com>
2025-02-01 21:47:41 +0530

    Update 12 String Slicing.py

--- 12 String Slicing.py
+++ 12 String Slicing.py
@@ -4,3 +4,8 @@ print(names[0:5])       # prints the length including 0 but not 5
 print(len(names))       # Gives the length of a string 
 print(fruit[0:4])       # prints the length including 0 but not 4
 
+#Negative slicing 
+print(fruit[:-3])                 #Python always includes len(fruit) -3 automatically
+print(fruit[0:len(fruit)-3])
+
+print(fruit[-3:-1])
