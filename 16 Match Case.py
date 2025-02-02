08347b8d47f09ce00eebd1b48ae9606c978eea25
Ashishat404 <138343378+Ashishat404@users.noreply.github.com>
2025-02-02 21:36:31 +0530

    Update 16 Match Case.py

--- 16 Match Case.py
+++ 16 Match Case.py
@@ -8,3 +8,12 @@ match x:
         # case with if-condition
     case 4:
         print("case is 4")
+    case _ if x!= 90 : 
+        print(x, "is not 90")
+        
+    case _ if x!= 80 : 
+        print(x, "is not 80")
+        
+# default case will only be matched if above cases were not matched  
+    case _:
+        print(x)
