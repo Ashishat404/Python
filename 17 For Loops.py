7d0e552d783c89b7211fdf5494545dfe92d17219
Ashishat404 <138343378+Ashishat404@users.noreply.github.com>
2025-02-08 08:28:48 +0530

    Update 17 For Loops.py

--- 17 For Loops.py
+++ 17 For Loops.py
@@ -9,3 +9,14 @@
 #     print(color)
 #     for i in color:
 #         print(i)
+
+# # range 
+# for k in range(5):
+#     print(k+1)
+    
+    
+# for k in range(1,20001):
+#     print(k)
+
+for k in range(1, 12, 3):
+    print(k)
