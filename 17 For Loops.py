a2b57af16b428e12bed4f712a8a136055dc13551
Ashishat404 <138343378+Ashishat404@users.noreply.github.com>
2025-02-08 08:28:17 +0530

    Update 17 For Loops.py

--- 17 For Loops.py
+++ 17 For Loops.py
@@ -1,1 +1,11 @@
+# name = 'Ashish'
+# for i in name:
+#     print (i)
+    
 
+    # This is how we iterate a list
+# colors = {"Red", "Green", "Blue", "Yellow"}
+# for color in colors:
+#     print(color)
+#     for i in color:
+#         print(i)
