d91361bfb12cdda42d202d397f8ce3f33143d2dc
Ashishat404 <138343378+Ashishat404@users.noreply.github.com>
2025-02-12 20:31:53 +0530

    Update 19 Break And Continue.py

--- 19 Break And Continue.py
+++ 19 Break And Continue.py
@@ -3,3 +3,7 @@ for i in range(12):
        print(" Skip The Iteration")
        continue
     print("5 X ", i, "=" ,  5* (i))
+# jese hi i=9 run hoga value 50 milegi aur jab i=10 ho jayega tab tab usse break statement mil jayega --> Loop Ko chodkar Nikal jao
+
+# break -> loop ko chodkar nikal jao
+# continue -> iteration ko chodkar nikal jao
