3c32e5548204d82d74169616e68d997cd804df60
Ashishat404 <138343378+Ashishat404@users.noreply.github.com>
2025-02-12 21:05:47 +0530

    Update 20 Functions.py

--- 20 Functions.py
+++ 20 Functions.py
@@ -1,1 +1,12 @@
+def calculateGmean(a,b):
+    mean = (a*b)/(a+b)
+    print(mean)
 
+def isGreater(a,b):
+   if(a>b):
+    print("First number is greater")
+   else:
+    print("Second number is greater")
+         
+def isLesser(a,b):
+    pass 
