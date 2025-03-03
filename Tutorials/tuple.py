19bbe4df9e30c98f249710a657356a84048cd95a
Ashishat404 <138343378+Ashishat404@users.noreply.github.com>
2025-03-03 21:40:43 +0530

    Update tuple.py

--- Tutorials/tuple.py
+++ Tutorials/tuple.py
@@ -1,2 +1,6 @@
 myTuple = (1, "Hello", True, 3.14, [1, 2, 3])
 print(myTuple)
+
+# Accessing values in tuple
+print("\nAccessing values in tuple")
+print(myTuple[0])   # Output : 1, zero-based indexing
