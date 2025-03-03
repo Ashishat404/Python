33a5ba2c8b1c31d1e1d36a47a922b2724d055bba
Ashishat404 <138343378+Ashishat404@users.noreply.github.com>
2025-03-03 21:42:08 +0530

    Update tuple.py

--- Tutorials/tuple.py
+++ Tutorials/tuple.py
@@ -6,3 +6,6 @@ print("\nAccessing values in tuple")
 print(myTuple[0])   # Output : 1, zero-based indexing
 print(myTuple[3])   # Output : 3.14
 print(myTuple[-1])  # Output : [1, 2, 3], Print tuple from last
+
+# Deleting values in tuple
+print("\nDeleting values in tuple")
