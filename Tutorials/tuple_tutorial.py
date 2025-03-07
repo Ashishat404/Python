cd73426660ddbad53dad3b52a15804e5e8ef8428
Ashishat404 <138343378+Ashishat404@users.noreply.github.com>
2025-03-07 12:00:00 +0530

    Update tuple_tutorial.py

--- Tutorials/tuple_tutorial.py
+++ Tutorials/tuple_tutorial.py
@@ -45,3 +45,4 @@ print("Index of 88 in tuple : ",
 myTuple2.index(88))   # Output : 3 
 print("Sum of all elements in tuple 2 is 
 " ,sum(myTuple2))  # Output : 405
+print("List in sorted order : ", 
