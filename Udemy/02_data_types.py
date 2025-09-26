79df7837d97026a9dac14a3f488a8eb7d882fa6f
Ashishat404 <138343378+Ashishat404@users.noreply.github.com>
2025-09-26 19:27:59 +0530

    Update 02_data_types.py

--- Udemy/02_data_types.py
+++ Udemy/02_data_types.py
@@ -3,4 +3,6 @@
 #     Integers (int): Whole numbers (e.g., 10, -5).
 #     Floats (float): Decimal numbers (e.g., 3.14, -0.001).
 #     Strings (str): Text data enclosed in quotes (e.g., "Hello", 'Python').
-# 
+#     Booleans (bool): Represents True or False.
+#     Lists: Ordered, mutable collections (e.g., [1, 2, 3]).
+#
