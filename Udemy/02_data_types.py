e6e9af74dd36a412bc45cc3d337a6af4972f870d
Ashishat404 <138343378+Ashishat404@users.noreply.github.com>
2025-09-26 19:27:39 +0530

    Update 02_data_types.py

--- Udemy/02_data_types.py
+++ Udemy/02_data_types.py
@@ -1,4 +1,6 @@
 
 # Python supports several built-in data types:
 #     Integers (int): Whole numbers (e.g., 10, -5).
-#
+#     Floats (float): Decimal numbers (e.g., 3.14, -0.001).
+#     Strings (str): Text data enclosed in quotes (e.g., "Hello", 'Python').
+# 
