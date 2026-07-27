647ecacec7625cf5d5a0e6d35ae10fea26e4a7f6
Ashishat404 <ashishbhattat404@gmail.com>
2026-07-27 21:24:36 +0530

    Update Slugify.py

--- Udemy/Slugify.py
+++ Udemy/Slugify.py
@@ -1,1 +1,10 @@
 
+def slugify(text):
+    # Convert the entire string to lowercase
+    text = text.lower()
+
+    # This list will store the final characters
+    result = []
+
+    # This flag keeps track of whether the last character added was a dash
+    last_was_dash = False
