7e80aeb2a8c7d2392f20892d1a03422d8bb9cf02
Ashishat404 <ashishbhattat404@gmail.com>
2026-08-06 19:21:16 +0530

    Update Sets.py

--- Udemy/Sets.py
+++ Udemy/Sets.py
@@ -23,3 +23,7 @@ print(my_set.issubset(my_set2))  # Output : True
 # superset of sets
 print(my_set >= my_set2)  # Output : False
 print(my_set.issuperset(my_set2))  # Output : False
+
+
+# length of set
+print(len(my_set))  # Output : 5
