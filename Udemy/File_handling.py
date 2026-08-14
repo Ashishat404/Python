b28b09b966cb26b2b8508b996b59d5ebfd041770
Ashishat404 <ashishbhattat404@gmail.com>
2026-08-14 15:51:15 +0530

    Refactor file handling and remove redundant checks
    Removed file existence check for 'test.txt' and updated the renaming process.

--- Udemy/File_handling.py
+++ Udemy/File_handling.py
@@ -20,19 +20,13 @@ os.remove("test.txt")
 
 # checking if a file exists
 import os
-
 if os.path.exists("test.txt"):
     print("File exists")
 else:
     print("File does not exist")
     
-#Renaming a File
-import os 
-if os.path.exists("test"):
-    print("file exists")
-else:
-    print("file does not exist")
-
 #Renaming a file 
 import os 
 os.rename("test.txt, "test1.txt")
+
+#copying a file
